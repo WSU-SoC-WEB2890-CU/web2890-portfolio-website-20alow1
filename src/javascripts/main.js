4c4291b1d3f3784d0e19bacd3851dd01b0e2935c
Avrey Low <avreylow@gmail.com>
2024-11-12 20:17:25 -0700

    Just another commit

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,21 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
-import "bootstrap"
+import "bootstrap";
+
+
+// const hoverCards = document.querySelectorAll('.projectCard');
+// const elementsToDisplay = document.querySelectorAll('.projectHover');
+
+// // Add event listeners for hover events
+// hoverCards.forEach(card => {
+//   card.addEventListener('mouseenter', () => {
+//     elementsToDisplay.forEach(element => {
+//       element.style.display = 'block';
+//     });
+//   });
+
+//   card.addEventListener('mouseleave', () => {
+//     elementsToDisplay.forEach(element => {
+//       element.style.display = 'none';
+//     });
+//   });
+// });
